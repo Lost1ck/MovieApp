c3a80c6d089a59aaab3f0b11d062448863fe0211
Lost One <lostone@MacBook-Air-Lost.local>
2024-01-23 17:33:03 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/App.jsx';
 import './styles/style.css';
+import App from './components/App.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
